6c91ea402084f7cd390446d90ed3a1f05a7fef1f
Abricos78 <67517869+Abricos78@users.noreply.github.com>
2026-05-30 14:21:32 +0300

    Add: products-of-array-discluding-self - submission-4

---
+++ Data Structures & Algorithms/products-of-array-discluding-self/submission-4.ts
@@ -0,0 +1,34 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number[]}
+     */
+    productExceptSelf(nums: number[]): number[] {
+        let zeroCount = 0
+        let productAll = nums.reduce((acc, num) => {
+            if (num === 0) {
+                zeroCount++
+                return acc
+            }
+            return acc *= num
+        }, 1)
+
+        if (zeroCount > 1) {
+            return nums.map(() => 0)
+        }
+
+        if (zeroCount) {
+            return nums.reduce((acc, num) => {
+                if (num !== 0) acc.push(0)
+                else acc.push(productAll)
+
+                return acc
+            }, [])
+        }
+
+        return nums.reduce((acc, num) => {
+            acc.push(productAll / num)
+            return acc
+        }, [])
+    }
+}
