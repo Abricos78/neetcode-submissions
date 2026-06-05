8b1180783971b5047615a752e9352983bb7574f3
Abricos78 <67517869+Abricos78@users.noreply.github.com>
2026-06-05 10:25:03 +0300

    Add: find-minimum-in-rotated-sorted-array - submission-5

---
+++ Data Structures & Algorithms/find-minimum-in-rotated-sorted-array/submission-5.ts
@@ -0,0 +1,51 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number}
+     */
+    findMin(nums: number[]): number {
+        if(nums.length < 3) {
+            return Math.min(...nums)
+        }
+
+        let start = 0
+        let end = nums.length - 1
+        let middle = Math.floor(end / 2)
+    
+        if (nums[start] > nums[middle] || nums[middle] > nums[end]) {
+            while(middle > start) {
+                if (nums[middle] < nums[middle - 1]) return nums[middle]
+                if (nums[middle] > nums[middle + 1]) return nums[middle + 1]
+
+                if (nums[middle] < nums[start]) {
+                    end = middle
+                    middle = Math.floor((end - start) / 2) + start
+                } else {
+                    start = middle
+                    middle = Math.floor((end - start) / 2) + start
+                }
+            }
+        }
+
+        // start = Math.floor((nums.length - 1) / 2)
+        // end = nums.length - 1
+        // middle = Math.floor((end - start) / 2) + start
+
+        // if (nums[start] > nums[middle] || nums[middle] > nums[end]) {
+        //     while(middle < end) {
+        //         if (nums[middle] < nums[middle - 1]) return nums[middle]
+        //         if (nums[middle] > nums[middle + 1]) return nums[middle + 1]
+
+        //         if (nums[middle] > nums[end]) {
+        //             start = middle
+        //             middle = Math.floor((end - start) / 2) + start
+        //         } else {
+        //             end = middle
+        //             middle = Math.floor((end - start) / 2) + start
+        //         }
+        //     }
+        // }
+
+        return nums[0]
+    }
+}
